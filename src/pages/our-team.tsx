c63637bba7763f2503b345c66a968f235c87b055
akshat2048 <77075876+akshat2048@users.noreply.github.com>
2022-06-12 11:32:00 -0500

    added ppl to our-team

--- src/pages/our-team.tsx
+++ src/pages/our-team.tsx
@@ -57,13 +57,58 @@ const articles = [
     alt: `Proident pariatur est.`,
     name: `Co-Founder`,
   },
+];
+
+const tentative_staff_articles = [
   {
     title: `Rafeh Khan something soemthing something someething. something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something  `,
     image: `/images/case-2.webp`,
     alt: `Proident pariatur est.`,
     name: `Lead Instructor`,
+  },
+  {
+    title: `Shailan Patel something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing  `,
+    image: `/images/case-1.webp`,
+    alt: `Proident pariatur est.`,
+    name: `Instructor`,
+  },
+  {
+    title: `Melinda Li something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing  `,
+    image: `/images/case-3.webp`,
+    alt: `Proident pariatur est.`,
+    name: `Lab Leader`,
+  },
+  {
+    title: `Claire Wang something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing  `,
+    image: `/images/case-1.webp`,
+    alt: `Proident pariatur est.`,
+    name: `Lab Leader`,
+  },
+  {
+    title: `Dheeraj Pasikanti something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something  `,
+    image: `/images/case-2.webp`,
+    alt: `Proident pariatur est.`,
+    name: `Instructor`,
+  },
+  {
+    title: `Mahit Dagar something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something  `,
+    image: `/images/case-3.webp`,
+    alt: `Proident pariatur est.`,
+    name: `Instructor`,
+  },
+  {
+    title: `Arnav Mehta something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something  `,
+    image: `/images/case-1.webp`,
+    alt: `Proident pariatur est.`,
+    name: `Instructor`,
+  },
+  {
+    title: `Aarush Jain something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something soemthing something someething something  `,
+    image: `/images/case-2.webp`,
+    alt: `Proident pariatur est.`,
+    name: `Instructor`,
   }
-];
+]
 
 const CasesSection = () => (
   <Page>
@@ -77,9 +122,42 @@ const CasesSection = () => (
         <p className={tw(`text-white text-gray-400 text-center text-xl mt-12`)}>
           Our camp leaders and instructors are picked from a pool of some of the best debaters in the nation.
         </p>
+        <p className={tw(`text-white text-gray-400 text-center text-xl mt-12`)}>
+          <b>Co-Founders</b>
+        </p>
         <div className={tw(`mx-auto pt-24`)}>
           <div className={tw(`w-full flex flex-wrap justify-around`)}>
             {articles.map((article) => (
+              <div
+                key={article.title}
+                className={tw(
+                  `xl:w-1/3 sm:w-5/12 sm:max-w-xs relative mb-32 lg:mb-20
+                      xl:max-w-sm lg:w-1/2 w-11/12 mx-auto sm:mx-0`,
+                )}
+              >
+                <div className={tw(`h-64 z-20`)}>
+                  <img
+                    src={article.image}
+                    alt={article.alt}
+                    className={tw(`h-full w-full object-cover overflow-hidden rounded`)}
+                    width={400}
+                    height={300}
+                  />
+                </div>
+                <div className={tw(`p-4 shadow-lg w-full mx-auto -mt-8 bg-white rounded-b z-30 relative`)}>
+                  <p className={tw(`uppercase text-sm text-gray-700 text-center pb-1`)}>{article.name}</p>
+                  <p className={tw(`text-gray-500 text-center pb-1 text-sm`)}>{article.title}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <p className={tw(`text-white text-gray-400 text-center text-xl mt-12`)}>
+          <b>Tentative Staff</b>
+        </p>
+        <div className={tw(`mx-auto pt-24`)}>
+          <div className={tw(`w-full flex flex-wrap justify-around`)}>
+            {tentative_staff_articles.map((article) => (
               <div
                 key={article.title}
                 className={tw(
