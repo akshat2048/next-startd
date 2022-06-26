20ec6ca5e0ef1e336acc046314cf1780a22cefc6
akshat2048 <77075876+akshat2048@users.noreply.github.com>
2022-06-26 16:55:43 -0500

    fixed feature section

--- src/components/feature-section/index.tsx
+++ src/components/feature-section/index.tsx
@@ -6,10 +6,10 @@ const FeatureSection = () => (
   <section className={tw(`bg-white pb-6`)}>
     <div className={tw(`max-w-7xl mx-auto p-4 sm:p-6 lg:p-8`)}>
       <div className={tw(`container mx-auto px-6 p-6 bg-white`)}>
-        <div className={tw(`mb-16 text-center`)}>
-          <h4 className={tw(`text-base text-indigo-600 font-semibold tracking-wide uppercase`)}>Features</h4>
+        <div className={tw(`mb-16 text-left`)}>
+          <h4 className={tw(`text-base text-indigo-600 font-semibold tracking-wide uppercase`)}>Our Mission</h4>
           <p className={tw(`mt-2 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900`)}>
-            How we change the game
+            Why Impact?
           </p>
         </div>
         <div className={tw(`flex flex-wrap my-12`)}>
@@ -31,15 +31,27 @@ const FeatureSection = () => (
             Unbeknownst to many that enter the activity, debate is an expensive extracurricular. The emergence of private coaching and costly prep groups exacerbate financial inequalities. Compared to the thousands of dollars our competitors charge, our camp is priced at $30 per student for 2 weeks of instruction. Our goal is to reduce resource inequities in debate, and we encourage our students to reach out to our staff even after the camp ends for an extra help needed without charge. 100% of the profits from our camp will go toward the <a href="https://www.covenanthouse.org" className={tw(`underline text-blue-600 hover:text-blue-800 visited:text-purple-600`)}>Covenant House</a>, a charity which provides shelter, food, immediate crisis care, and other services to underprivileged youth. 
             </p>
           </div>
-          <div className={tw(`w-full border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0 p-8`)}>
-            <div className="flex items-center mb-6">
+          {/* <div className={tw(`w-full md:w-1/2 lg:w-1/3 p-8`)}>
+            <div className={tw(`flex items-center mb-6`)}>
               <Check width={20} height={20} fill="currentColor" className={tw(`h-6 w-6 text-indigo-500`)} />
-              <div className={tw(`ml-4 text-xl`)}>Why Debate?</div>
+              <div className={tw(`ml-4 text-xl`)}>Integrations</div>
             </div>
             <p className={tw(`leading-loose text-gray-500`)}>
-            Many parents and students who are considering joining their high school debate team often wonder how beneficial the activity will truly be. The center of competitive high school debate is to research both sides of important issues in a respectful medium for discussion. Students often devote countless hours researching arguments, writing cases, practicing speeches, and developing their ability to think on their feet. As a result, high school debaters reap numerous benefits. 
+              Nostrud excepteur incididunt proident sit nulla ipsum sunt nostrud est esse adipisicing irure officia
+              consectetur.
             </p>
-          </div>
+          </div> */}
+        </div>
+        
+        <p className={tw(`mt-2 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900`)}>
+            Why Debate?
+          </p>
+          <p className={tw(`text-base text-gray-600 tracking-wide uppercase`)}>- </p>
+        <p className={tw(` text-gray-500 tracking-wide`)}>
+        Many parents and students who are considering joining their high school debate team often wonder how beneficial the activity will truly be. The center of competitive high school debate is to research both sides of important issues in a respectful medium for discussion. Students often devote countless hours researching arguments, writing cases, practicing speeches, and developing their ability to think on their feet. As a result, high school debaters reap numerous benefits. 
+
+        </p>
+        <div className={tw(`flex flex-wrap my-12`)}>
           <div className={tw(`w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8`)}>
             <div className={tw(`flex items-center mb-6`)}>
               <Check width={20} height={20} fill="currentColor" className={tw(`h-6 w-6 text-indigo-500`)} />
